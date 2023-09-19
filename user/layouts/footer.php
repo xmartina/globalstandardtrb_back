98cb60c0f6616f9535afb269f3e9e480a72b215f
unknown <martinsprince126@gmail.com>
2023-09-19 15:52:04 -0400

    updated

--- user/layouts/footer.php
+++ user/layouts/footer.php
@@ -3,7 +3,7 @@
 </div>
 <div class="footer-wrapper">
     <div class="footer-section f-section-1">
-        <p class="">Copyright © 2021 <?php echo $pageTitle ?>, All rights reserved.</p>
+        <p class="">Copyright © 2023 <?php echo $pageTitle ?>, All rights reserved.</p>
     </div>
     <div class="footer-section f-section-2">
         <p class=""><?php echo $pageTitle ?> </p>
